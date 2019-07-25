f6db19213c5257346e378d3fc0a427d3c87a880a
Evan Coulson <evan.m.coulson@gmail.com>
2019-07-25 00:24:47 -0700

    added decleartions to package and versioned release

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import IRouteHandler from "./Router/IRouteHandler";
+import IRouter from "./Router/IRouter";
+import Router from "./Router/Router";
+import StatusCodes from './Router/StatusCodes';
+import IMessage from './Messages/IMessage';
+import Message from './Messages/Message';
+
+export {
+    IRouteHandler,
+    IRouter,
+    Router,
+    StatusCodes,
+    IMessage,
+    Message
+};
